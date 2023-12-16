8710b3b596e92d4df062bc37b97045c02f5a29a7
ize-302 <adavizeozorku@gmail.com>
2023-12-16 17:05:57 +0100

    feat: update app

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send({ greetings: 'Hello World!' })
+  return res.status(200).json({
+    message: 'Hello World'
+  });
 })
 
 app.listen(port, () => {
